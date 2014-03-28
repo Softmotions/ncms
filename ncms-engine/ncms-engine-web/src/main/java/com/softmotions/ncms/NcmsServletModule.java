727031b08d148663be377db30d3d15055ab2e256
Антон Адаманский <adamansky@gmail.com>
2014-03-28 15:20:45 +0700

    ncms

--- ncms-engine/ncms-engine-web/src/main/java/com/softmotions/ncms/NcmsServletModule.java
+++ ncms-engine/ncms-engine-web/src/main/java/com/softmotions/ncms/NcmsServletModule.java
@@ -2,11 +2,20 @@ package com.softmotions.ncms;
 
 import ninja.servlet.NinjaServletDispatcher;
 import ninja.utils.NinjaProperties;
+import com.softmotions.commons.web.JarResourcesProvider;
+import com.softmotions.commons.web.JarResourcesServlet;
 
+import com.google.inject.Singleton;
 import com.google.inject.servlet.ServletModule;
 
+import org.apache.commons.configuration.HierarchicalConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServlet;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * NCMS engine servlet module.
@@ -27,13 +36,54 @@ public class NcmsServletModule extends ServletModule {
                                        "ServletContext#" + NcmsServletListener.NCMS_NINJA_PROPS_SCTX_KEY
                                        + " attribute");
         }
+        String ncmsCfgFile = nprops.get("ncms.configurationFile");
+        if (ncmsCfgFile == null) {
+            log.warn("Missing 'ncms.configurationFile' property in the ninja configuration, " +
+                     "using fallback resource location: " + NcmsConfiguration.DEFAULT_CFG_RESOURCE);
+        }
 
-        initNinja();
+        //Bind NcmsConfiguration
+        NcmsConfiguration ncmsCfg = new NcmsConfiguration(nprops, ncmsCfgFile, true);
+        bind(NcmsConfiguration.class).toInstance(ncmsCfg);
+
+        initServlets(ncmsCfg);
     }
 
-    protected void initNinja() {
+    protected void initServlets(NcmsConfiguration cfg) {
+
         //Ninja init part
         bind(NinjaServletDispatcher.class).asEagerSingleton();
-        serve("/*").with(NinjaServletDispatcher.class);
+        serve(cfg.getNcmsPrefix() + "/exec/*", NinjaServletDispatcher.class);
+        serve(cfg.getNcmsPrefix() + "/admin/exec/*", NinjaServletDispatcher.class);
+
+        initJarResourcesServlet(cfg);
     }
+
+    protected void initJarResourcesServlet(NcmsConfiguration cfg) {
+        Map<String, String> params = new LinkedHashMap<>();
+        List<HierarchicalConfiguration> rlist = cfg.impl().configurationsAt("jar-web-resources.resource");
+        for (HierarchicalConfiguration rcfg : rlist) {
+            String pp = rcfg.getString("path-prefix");
+            String opts = rcfg.getString("options");
+            if (pp == null || opts == null) {
+                continue;
+            }
+            params.put(pp, opts);
+        }
+        bind(JarResourcesServlet.class).in(Singleton.class);
+        bind(JarResourcesProvider.class).to(JarResourcesServlet.class);
+        serve(cfg.getNcmsPrefix() + "/*", JarResourcesServlet.class, params);
+
+    }
+
+    protected void serve(String pattern, Class<? extends HttpServlet> servletClass) {
+        log.info("Serving {} with {}", pattern, servletClass);
+        serve(pattern).with(servletClass);
+    }
+
+    protected void serve(String pattern, Class<? extends HttpServlet> servletClass, Map<String, String> params) {
+        log.info("Serving {} with {}", pattern, servletClass);
+        serve(pattern).with(servletClass, params);
+    }
+
 }
