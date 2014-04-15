ee86676ac441b59551570bdefd55beb5a8a7f093
Антон Адаманский <adamansky@gmail.com>
2014-04-15 15:38:49 +0700

    tested SecurityContext with resteasy

--- ncms-engine/ncms-engine-web/src/main/java/com/softmotions/ncms/adm/WorkspaceRS.java
+++ ncms-engine/ncms-engine-web/src/main/java/com/softmotions/ncms/adm/WorkspaceRS.java
@@ -3,6 +3,8 @@ package com.softmotions.ncms.adm;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.SecurityContext;
 
 /**
  * @author Adamansky Anton (adamansky@gmail.com)
@@ -11,15 +13,10 @@ import javax.ws.rs.PathParam;
 @Path("adm/ws")
 public class WorkspaceRS {
 
-
-
-
-
-
-
     @GET
     @Path("hello/{name}")
-    public String hello(@PathParam("name") final String name) {
-        return "Hello " + name;
+    public String hello(@PathParam("name") final String name,
+                        @Context SecurityContext sctx) {
+        return "Hello " + name + " you are logged in as: " + sctx.getUserPrincipal();
     }
 }
