fad1ce9171a48b3d02a9494316a42a66567711d0
Alexander Osipov <alexander@onpositive.com>
2014-04-22 17:58:27 +0700

    fix binding

--- ncms-engine/ncms-engine-web/src/main/java/com/softmotions/ncms/media/db/MediaDbModule.java
+++ ncms-engine/ncms-engine-web/src/main/java/com/softmotions/ncms/media/db/MediaDbModule.java
@@ -2,6 +2,7 @@ package com.softmotions.ncms.media.db;
 
 import com.google.inject.AbstractModule;
 import com.softmotions.ncms.media.model.MediaFile;
+import com.softmotions.ncms.media.model.Tag;
 
 /**
  * Created by shu on 4/19/2014.
@@ -10,5 +11,6 @@ public class MediaDbModule extends AbstractModule {
   @Override
   protected void configure() {
     bind(MediaFile.class);
+    bind(Tag.class);
   }
 }
