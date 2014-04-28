0cc0a517ba24e498f3e76be8ecb6b95eeb084e43
Alexander Osipov <alexander@onpositive.com>
2014-04-28 18:05:21 +0700

    files list test

--- ncms-engine/ncms-engine-web/src/main/java/com/softmotions/ncms/media/rest/MediaFileRS.java
+++ ncms-engine/ncms-engine-web/src/main/java/com/softmotions/ncms/media/rest/MediaFileRS.java
@@ -12,6 +12,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
+import java.util.List;
 
 /**
  * Created by shu on 4/23/2014.
