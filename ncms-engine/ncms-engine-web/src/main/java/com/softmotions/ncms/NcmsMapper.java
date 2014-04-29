4718825b70f40688b96c466f57bb6cdc4540d714
Дмитрий Савельев <dd.savelev@gmail.com>
2014-04-29 19:29:04 +0700

    реализован, но не протестирован NcmsSecurityRS

---
+++ ncms-engine/ncms-engine-web/src/main/java/com/softmotions/ncms/NcmsMapper.java
@@ -0,0 +1,22 @@
+package com.softmotions.ncms;
+
+import com.softmotions.web.security.WSRole;
+import com.softmotions.web.security.WSUser;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import java.util.Iterator;
+
+/**
+ * Created by dimad on 29.04.14.
+ */
+public class NcmsMapper {
+
+    private NcmsMapper(){
+    }
+
+
+
+}
--- ncms-engine/ncms-engine-web/src/main/java/com/softmotions/ncms/security/NcmsSecurityRS.java
+++ ncms-engine/ncms-engine-web/src/main/java/com/softmotions/ncms/security/NcmsSecurityRS.java
@@ -1,5 +1,7 @@
 package com.softmotions.ncms.security;
 
+import com.softmotions.web.security.WSGroup;
+import com.softmotions.web.security.WSRole;
 import com.softmotions.web.security.WSUser;
 import com.softmotions.web.security.WSUserDatabase;
 
@@ -14,6 +16,8 @@ import javax.ws.rs.NotFoundException;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import java.util.Iterator;
 
 /**
  * Rest service for operations on
@@ -81,10 +85,286 @@ public class NcmsSecurityRS {
         return userDatabase.getUsersCount();
     }
 
-    //todo
-    //
-    //
-    //
-    //
+
+    /**
+     * Get group info list
+     * <pre>
+     *  [{
+     *      "name":"admins",
+     *      "description":"Superuser group"
+     *  }]
+     * </pre>
+     *
+     * @return
+     */
+    @GET
+    @Path("groups")
+    public JsonNode groups() {
+        Iterator<WSGroup> groups = userDatabase.getGroups();
+        ArrayNode res = mapper.createArrayNode();
+        while (groups.hasNext()) {
+            WSGroup g = groups.next();
+            res.addObject()
+                    .put("name", g.getName())
+                    .put("description", g.getDescription());
+        }
+        return res;
+    }
+
+    /**
+     * Get role info list
+     * <pre>
+     *  [{
+     *      "name":"admins",
+     *      "description":"Superuser group"
+     *  }]
+     * </pre>
+     *
+     * @return
+     */
+    @GET
+    @Path("roles")
+    public JsonNode roles() {
+        Iterator<WSRole> roles = userDatabase.getRoles();
+        ArrayNode res = mapper.createArrayNode();
+        while (roles.hasNext()) {
+            WSRole role = roles.next();
+            res.addObject()
+                    .put("name", role.getName())
+                    .put("description", role.getDescription());
+        }
+        return res;
+    }
+
+    /**
+     * Get users info list
+     * <pre>
+     *  [{
+     *      "name":"admins",
+     *      "description":"Superuser group"
+     *  }]
+     * </pre>
+     *
+     * @return
+     */
+    @GET
+    @Path("users")
+    public JsonNode users() {
+        Iterator<WSUser> users = userDatabase.getUsers();
+        ArrayNode res = mapper.createArrayNode();
+        while (users.hasNext()) {
+            WSUser user = users.next();
+            res.addObject()
+                    .put("name", user.getName())
+                    .put("email", user.getEmail())
+                    .put("fullName", user.getFullName());
+        }
+        return res;
+    }
+
+    /**
+     * Get users info list from firstRow to firstRow+count
+     * <pre>
+     *  [{
+     *      "name":"admins",
+     *      "description":"Superuser group"
+     *  }]
+     * </pre>
+     *
+     * @return
+     */
+    @GET
+    @Path("users")
+    public JsonNode users(@QueryParam("firstRow") int firstRow, @QueryParam("lastRow") int lastRow) {
+        Iterator<WSUser> users = userDatabase.getUsers(firstRow, Math.abs(lastRow - firstRow) + 1);
+        ArrayNode res = mapper.createArrayNode();
+        while (users.hasNext()) {
+            WSUser user = users.next();
+            res.addObject()
+                    .put("name", user.getName())
+                    .put("email", user.getEmail())
+                    .put("fullName", user.getFullName());
+        }
+        return res;
+    }
+
+    /**
+     * Creates group with name and description
+     *
+     * @return
+     * <pre>
+     *  {
+     *      "name":"admins",
+     *      "description":"Superuser group"
+     *  }
+     * </pre>
+     */
+    @GET
+    @Path("creategroup")
+    public JsonNode createGroup(@QueryParam("name") String name, @QueryParam("description") String description) {
+        WSGroup group = userDatabase.createGroup(name, description);
+        ObjectNode res = mapper.createObjectNode();
+        res.put( "name", group.getName());
+        res.put( "description", group.getDescription());
+        return res;
+    }
+
+    /**
+     * Creates role with name and description
+     *
+     * @return
+     * <pre>
+     *  {
+     *      "name":"admins",
+     *      "description":"Description of role"
+     *  }
+     * </pre>
+     */
+    @GET
+    @Path("createrole")
+    public JsonNode createRole(@QueryParam("rolename") String rolename, @QueryParam("description") String description) {
+        WSRole role = userDatabase.createRole(rolename, description);
+        ObjectNode res = mapper.createObjectNode();
+        res.put( "name", role.getName());
+        res.put( "description", role.getDescription());
+        return res;
+    }
+
+    /**
+     * Creates user with name, password and full name
+     *
+     * @return
+     * <pre>
+     *  {
+     *      "name":"admin",
+     *      "password":"password",
+     *      "fullName":"Иванов Иван Иванович"
+     *  }
+     * </pre>
+     */
+    @GET
+    @Path("createuser")
+    public JsonNode createUser(@QueryParam("name") String name, @QueryParam("password") String password, @QueryParam("fullname") String fullName) {
+        WSUser user = userDatabase.createUser(name,password,fullName);
+        ObjectNode res = mapper.createObjectNode();
+        res.put( "name", user.getName());
+        res.put( "password", user.getPassword());
+        res.put( "fullName", user.getFullName());
+        return res;
+    }
+
+    /**
+     * Finds group of the name groupName
+     * <pre>
+     *  {
+     *      "name":"admins",
+     *      "description":"Superuser group"
+     *  }
+     * </pre>
+     *
+     * @return
+     */
+    @GET
+    @Path("findgroup")
+    public JsonNode findGroup(@QueryParam("groupname") String groupName) {
+        WSGroup group = userDatabase.findGroup(groupName);
+        ObjectNode res = null;
+        if(group != null) {
+            res = mapper.createObjectNode();
+            res.put("name", group.getName())
+                    .put("description", group.getDescription());
+        }
+        return res;
+    }
+
+    /**
+     * Finds role of the name rolename
+     * <pre>
+     *  {
+     *      "name":"admins",
+     *      "description":"Superuser role"
+     *  }
+     * </pre>
+     *
+     * @return
+     */
+    @GET
+    @Path("findrole")
+    public JsonNode findRole(@QueryParam("rolename") String roleName) {
+        WSRole role = userDatabase.findRole(roleName);
+        ObjectNode res = null;
+        if(role != null) {
+            res = mapper.createObjectNode();
+            res
+                    .put("name", role.getName())
+                    .put("description", role.getDescription());
+        }
+        return res;
+    }
+
+    /**
+     * Finds user of the name username
+     * <pre>
+     *  {
+     *      "name":"admin",removegroup
+     *      "description":"Superuser"
+     *  }
+     * </pre>
+     *
+     * @return
+     */
+    @GET
+    @Path("finduser")
+    public JsonNode findUser(@QueryParam("username") String username) {
+        WSUser user = userDatabase.findUser(username);
+        ObjectNode res = null;
+        if(user != null) {
+            res = mapper.createObjectNode();
+            res
+                    .put("name", user.getName())
+                    .put("fullName", user.getFullName())
+                    .put("email", user.getEmail());
+        }
+        return res;
+    }
+
+    /**
+     * Removes group of the name groupname
+     */
+    @GET
+    @Path("removegroup")
+    public void removeGroup(@QueryParam("name") String name) {
+        WSGroup group = userDatabase.findGroup(name);
+        if(group != null){
+            userDatabase.removeGroup(group);
+        }
+    }
+
+    /**
+     * Removes role of the name rolename
+     */
+    @GET
+    @Path("removerole")
+    public void removeRole(@QueryParam("name") String name) {
+        WSRole role = userDatabase.findRole(name);
+        if(role != null){
+            userDatabase.removeRole(role);
+        }
+    }
+
+    /**
+     * Removes user of the name username
+     */
+    @GET
+    @Path("removeuser")
+    public void removeUser(@QueryParam("name") String name) {
+        WSUser user = userDatabase.findUser(name);
+        System.out.println("user = "+user);
+        if(user != null){
+            userDatabase.removeUser(user);
+        }
+    }
+
+
 }
 
