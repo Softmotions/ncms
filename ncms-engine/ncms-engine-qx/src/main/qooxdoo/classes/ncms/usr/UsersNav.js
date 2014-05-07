8dfde57d543dff30b91516be05daeff73363cd4f
Дмитрий Савельев <dd.savelev@gmail.com>
2014-05-07 12:06:50 +0700

    Прикручиваем сервисы администрирования к таблице

--- ncms-engine/ncms-engine-qx/src/main/qooxdoo/classes/ncms/usr/UsersNav.js
+++ ncms-engine/ncms-engine-qx/src/main/qooxdoo/classes/ncms/usr/UsersNav.js
@@ -8,30 +8,152 @@ qx.Class.define("ncms.usr.UsersNav", {
     },
 
     events : {
+
+        /**
+         * Event fired if assembly was selected/deselected
+         *
+         * DATA: var item = {
+         *        "id" : {Number} Assembly id.
+         *        "name" : {String} Page name,
+         *        "type" : {String} Assembly type,
+         *       };
+         * or null if selection cleared
+         */
+        "asmSelected" : "qx.event.type.Data"
     },
 
     properties : {
+
+        appearance : {
+            refine : true,
+            init : "asm-selector"
+    	},
+
+        constViewSpec : {
+            check : "Object",
+            nullable : true,
+            apply : "__applyConstViewSpec"
+        }
     },
 
-    construct : function() {
-        this.base(arguments);
-        this._setLayout(new qx.ui.layout.Grow());
-
-        //todo remove it
-        this.setBackgroundColor("#ffffcc");
-
+    construct : function(constViewSpec, smodel) {
         //todo use example: ncms.asm.AsmSelector
         //todo use sm.ui.form.SearchField as search box
         //todo use ncms.usr.UsersTable (example: ncms.asm.AsmTable)
         //todo use com.softmotions.ncms.security.NcmsSecurityRS service
 
+        this.base(arguments);
+        this._setLayout(new qx.ui.layout.Dock());
+
+        var sf = this.__sf = new sm.ui.form.SearchField();
+        sf.addListener("clear", function() {
+            this.__search(null);
+        }, this);
+        sf.addListener("input", function(ev) {
+            this.__search(ev.getData());
+        }, this);
+        sf.addListener("changeValue", function(ev) {
+            this.__search(ev.getData());
+        }, this);
+
+
+        this.__table = new ncms.usr.UsersTable().set({
+            "statusBarVisible" : false,
+            "showCellFocusIndicator" : false});
+
+        if (smodel != null) {
+            this.__table.setSelectionModel(smodel);
+        }
+        this.__table.getSelectionModel().addListener("changeSelection", function() {
+            var asm = this.getSelectedAsm();
+            this.fireDataEvent("asmSelected", asm ? asm : null);
+        }, this);
+
+        this._add(this.__sf, {edge:"north"});
+        this._add(this.__table, {edge:"center"});
+
+        this.setConstViewSpec(constViewSpec || null);
     },
 
     members : {
 
+        /**
+         * Search field
+         * @type {sm.ui.form.SearchField}
+         */
+        __sf : null,
+
+        /**
+         * Assemblies virtual table
+         * @type {ncms.asm.AsmTable}
+         */
+        __table : null,
+
+
+        setViewSpec : function(vspec) {
+            this.__table.getTableModel().setViewSpec(this.__createViewSpec(vspec));
+        },
+
+        updateViewSpec : function(vspec) {
+            this.__table.getTableModel().updateViewSpec(this.__createViewSpec(vspec));
+        },
+
+        reload : function(vspec) {
+            this.__table.getTableModel().reloadData();
+            this.__table.resetSelection();
+        },
+
+        resetSelection : function() {
+            this.__table.resetSelection();
+        },
+
+        getTable : function() {
+            return this.__table;
+        },
+
+        getSelectedAsmInd : function() {
+            return this.__table.getSelectedAsmInd();
+        },
+
+        getSelectedAsm : function() {
+            return this.__table.getSelectedAsm();
+        },
+
+        getSelectedAsms : function() {
+            return this.__table.getSelectedAsms();
+        },
+
+        cleanup : function() {
+            this.__table.cleanup();
+        },
+
+        getSearchField : function() {
+            return this.__sf;
+        },
+
+        __createViewSpec : function(vspec) {
+            if (this.getConstViewSpec() == null) {
+                return vspec;
+            }
+            var nspec = {};
+            qx.Bootstrap.objectMergeWith(nspec, this.getConstViewSpec(), false);
+            qx.Bootstrap.objectMergeWith(nspec, vspec, false);
+            return nspec;
+        },
+
+        __search : function(val) {
+            this.__table.resetSelection();
+            var vspec = (val != null && val != "" ? {stext : val} : {});
+            this.setViewSpec(this.__createViewSpec(vspec));
+        },
+
+        __applyConstViewSpec : function() {
+            this.__search();
+        }
     },
 
     destruct : function() {
-        //this._disposeObjects("__field_name");                                
+        this.__sf = null;
+        this.__table = null;
     }
 });
---
+++ ncms-engine/ncms-engine-qx/src/main/qooxdoo/classes/ncms/usr/UsersTable.js
@@ -0,0 +1,72 @@
+/**
+ * Virtual table of assemblies.
+ */
+qx.Class.define("ncms.usr.UsersTable", {
+    extend : sm.table.Table,
+
+    construct : function(useColumns) {
+        var tm = new sm.model.RemoteVirtualTableModel({
+            "name" : this.tr("Name"),
+            "type" : this.tr("Type")
+        }).set({
+                    "useColumns" : useColumns || ["name", "type"],
+                    "rowdataUrl" : ncms.Application.ACT.getUrl("asms.select"),
+                    "rowcountUrl" : ncms.Application.ACT.getUrl("asms.select.count")
+                });
+
+        var custom = {
+            tableColumnModel : function(obj) {
+                return new qx.ui.table.columnmodel.Resize(obj);
+            }
+        };
+
+        this.base(arguments, tm, custom, true);
+
+        var rr = new sm.table.renderer.CustomRowRenderer();
+        var colorm = qx.theme.manager.Color.getInstance();
+        rr.setBgColorInterceptor(qx.lang.Function.bind(function(rowInfo) {
+            return colorm.resolve("background");
+        }, this));
+        this.setDataRowRenderer(rr);
+
+
+        var tcm = this.getTableColumnModel();
+        var cInd = tm.getColumnIndexById("name");
+        if (cInd != null) {
+            tcm.getBehavior().setWidth(cInd, "2*");
+        }
+        cInd = tm.getColumnIndexById("type");
+        if (cInd != null) {
+            tcm.getBehavior().setWidth(cInd, "1*");
+        }
+    },
+
+    members : {
+
+        getSelectedAsmInd : function() {
+            return this.getSelectionModel().getAnchorSelectionIndex();
+        },
+
+        getSelectedAsm : function() {
+            var sind = this.getSelectedAsmInd();
+            return sind != -1 ? this.getTableModel().getRowData(sind) : null;
+        },
+
+        getSelectedAsms : function() {
+            var me = this;
+            var asms = [];
+            this.getSelectionModel().iterateSelection(function(ind) {
+                asms.push(me.getTableModel().getRowData(ind));
+            });
+            return asms;
+        },
+
+        cleanup : function() {
+            this.getTableModel().cleanup();
+        }
+    },
+
+    destruct : function() {
+        //this._disposeObjects("__field_name");
+    }
+});
