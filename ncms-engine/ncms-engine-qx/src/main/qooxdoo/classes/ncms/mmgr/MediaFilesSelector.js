d41479a6d3e51e3e325b986aa2e84578f6c3a175
Антон Адаманский <adamansky@gmail.com>
2014-05-11 23:46:09 +0700

    #1 media gui

--- ncms-engine/ncms-engine-qx/src/main/qooxdoo/classes/ncms/mmgr/MediaFilesSelector.js
+++ ncms-engine/ncms-engine-qx/src/main/qooxdoo/classes/ncms/mmgr/MediaFilesSelector.js
@@ -1,6 +1,9 @@
 /**
  * Media files selector.
  * File list table with search field.
+ *
+ * @asset(ncms/icon/16/actions/add.png)
+ * @asset(ncms/icon/16/actions/delete.png)
  */
 qx.Class.define("ncms.mmgr.MediaFilesSelector", {
     extend : qx.ui.core.Widget,
@@ -35,10 +38,25 @@ qx.Class.define("ncms.mmgr.MediaFilesSelector", {
             check : "Object",
             nullable : true,
             apply : "__applyConstViewSpec"
+        },
+
+        /**
+         * Set media item:
+         *
+         *  item = {
+         *        "label"  : {String} Item name.
+         *        "status" : {Number} Item status. (1 - folder, 0 - file)
+         *        "path"   : {Array} Path to the item (from tree root)
+         *  };
+         */
+        "item" : {
+            check : "Object",
+            nullable : true,
+            apply : "__applyItem"
         }
     },
 
-    construct : function(constViewSpec, smodel) {
+    construct : function(allowModify, constViewSpec, smodel) {
         this.base(arguments);
         this._setLayout(new qx.ui.layout.VBox());
         var sf = this.__sf = new sm.ui.form.SearchField();
@@ -59,24 +77,40 @@ qx.Class.define("ncms.mmgr.MediaFilesSelector", {
         if (smodel != null) {
             this.__table.setSelectionModel(smodel);
         }
-        this.__table.getSelectionModel().addListener("changeSelection", function() {
+        this.__table.getSelectionModel().addListener("changeSelection", function(ev) {
+            this.__updateState();
             var file = this.__table.getSelectedFile();
             this.fireDataEvent("fileSelected", file ? file : null);
         }, this);
 
+
+        this.__allowModify = !!allowModify;
+        this.__dropFun = this.__handleDropFiles.bind(this);
+
         this._add(this.__sf);
+        this._setupToolbar();
         this._add(this.__table, {flex : 1});
 
         if (constViewSpec != null) {
             this.setConstViewSpec(constViewSpec);
         }
+        this.addListener("appear", function() {
+            this.__ensureUploadControls();
+            this.__updateState();
+        }, this);
     },
 
     members : {
 
+        __allowModify : false,
+
+        __dropFun : null,
+
         __sf : null,
 
         __table : null,
+
+        __rmBt : null,
 
         setViewSpec : function(vspec) {
             this.__table.getTableModel().setViewSpec(this.__createViewSpec(vspec));
@@ -99,6 +133,28 @@ qx.Class.define("ncms.mmgr.MediaFilesSelector", {
             return this.__table;
         },
 
+        _setupToolbar : function() {
+            if (!this.__allowModify) {
+                return;
+            }
+            var toolbar = new qx.ui.toolbar.ToolBar();
+            var part = new qx.ui.toolbar.Part()
+                    .set({"appearance" : "toolbar-table/part"});
+            toolbar.add(part);
+
+            var bt = new qx.ui.toolbar.Button(null, "ncms/icon/16/actions/add.png")
+                    .set({"appearance" : "toolbar-table-button"});
+            bt.addListener("execute", this.__addFiles, this);
+            part.add(bt);
+
+            this.__rmBt = bt = new qx.ui.toolbar.Button(null, "ncms/icon/16/actions/delete.png")
+                    .set({"appearance" : "toolbar-table-button"});
+            bt.addListener("execute", this.__rmFiles, this);
+
+            part.add(bt);
+            this._add(toolbar);
+        },
+
         __createViewSpec : function(vspec) {
             if (this.getConstViewSpec() == null) {
                 return vspec;
@@ -115,13 +171,73 @@ qx.Class.define("ncms.mmgr.MediaFilesSelector", {
             this.setViewSpec(this.__createViewSpec(vspec));
         },
 
+        __applyItem : function(item) {
+            if (item != null && item["status"] == 1) { //folder
+                var folder = "/" + item["path"].join("/");
+                this.setConstViewSpec({"folder" : folder, "status" : 0});
+            } else {
+                this.setConstViewSpec({"status" : 0});
+            }
+        },
+
         __applyConstViewSpec : function() {
             this.__search();
+        },
+
+        __addFiles : function(ev) {
+            qx.log.Logger.info("addFiles!!!");
+        },
+
+        __rmFiles : function(ev) {
+            qx.log.Logger.info("rmFiles!!!");
+        },
+
+        __updateState : function() {
+            var selected = !this.__table.getSelectionModel().isSelectionEmpty();
+            if (this.__rmBt) {
+                this.__rmBt.setEnabled(selected);
+            }
+        },
+
+        __handleDropFiles : function(ev) {
+            ev.stopPropagation();
+            ev.preventDefault();
+            var files = ev.dataTransfer.files;
+            var path = (this.getItem() != null) ? this.getItem()["path"] : [];
+            var dlg = new sm.ui.upload.FileUploadProgressDlg(function(f) {
+                return ncms.Application.ACT.getRestUrl("media.upload", path.concat(f.name));
+            }, files);
+            dlg.addListener("completed", function() {
+                dlg.close();
+                this.reload();
+            }, this);
+            dlg.open();
+        },
+
+        __ensureUploadControls : function() {
+            if (!this.__allowModify) { //we are in read-only mode
+                return;
+            }
+            var el = this.getContentElement().getDomElement();
+            if (el.ondrop == this.__dropFun) {
+                return;
+            }
+            el.ondrop = this.__dropFun;
+            el.ondragover = function() {
+                return false;
+            };
         }
     },
 
     destruct : function() {
+        if (this.getContentElement() != null) {
+            var el = this.getContentElement().getDomElement();
+            el.ondrop = null;
+            el.ondragover = null;
+        }
         this.__sf = null;
         this.__table = null;
+        this.__dropFun = null;
+        this.__rmBt = null;
     }
 });
--- ncms-engine/ncms-engine-qx/src/main/qooxdoo/classes/ncms/mmgr/MediaFolderEditor.js
+++ ncms-engine/ncms-engine-qx/src/main/qooxdoo/classes/ncms/mmgr/MediaFolderEditor.js
@@ -24,7 +24,7 @@ qx.Class.define("ncms.mmgr.MediaFolderEditor", {
          *  };
          */
         "item" : {
-            apply : "__applyItem",
+            event : "changeItem",
             nullable : true
         }
     },
@@ -33,7 +33,8 @@ qx.Class.define("ncms.mmgr.MediaFolderEditor", {
         this.base(arguments);
         this._setLayout(new qx.ui.layout.Grow());
 
-        var selector = this.__selector = new ncms.mmgr.MediaFilesSelector();
+        var selector = this.__selector = new ncms.mmgr.MediaFilesSelector(true);
+        this.bind("item", selector, "item");
         var editor = this.__editor = new ncms.mmgr.MediaFileEditor();
         selector.bind("fileSelected", editor, "fileSpec");
 
@@ -41,14 +42,6 @@ qx.Class.define("ncms.mmgr.MediaFolderEditor", {
         hsp.add(selector, 1);
         hsp.add(editor, 1);
         this._add(hsp);
-
-        this.__dropFun = this.__dropFiles.bind(this);
-        this.addListener("appear", function() {
-            if (this.getItem() == null) {
-                this.__applyItem(null);
-            }
-            this.__ensureUploadControls();
-        }, this);
     },
 
     members : {
@@ -57,51 +50,10 @@ qx.Class.define("ncms.mmgr.MediaFolderEditor", {
 
         __editor : null,
 
-        __dropFun : null,
-
-        __applyItem : function(item) {
-            if (item != null && item["status"] == 1) { //folder
-                var folder = "/" + item["path"].join("/");
-                this.__selector.setConstViewSpec({"folder" : folder, "status" : 0});
-            } else {
-                this.__selector.setConstViewSpec({"status" : 0});
-            }
-        },
-
-        __dropFiles : function(ev) {
-            ev.stopPropagation();
-            ev.preventDefault();
-            var files = ev.dataTransfer.files;
-            var path = (this.getItem() != null) ? this.getItem()["path"] : [];
-            var dlg = new sm.ui.upload.FileUploadProgressDlg(function(f) {
-                return ncms.Application.ACT.getRestUrl("media.upload", path.concat(f.name));
-            }, files);
-            dlg.addListener("completed", function() {
-                dlg.close();
-                this.__selector.reload();
-            }, this);
-            dlg.open();
-
-        },
-
-        __ensureUploadControls : function() {
-            var el = this.__selector.getContentElement().getDomElement();
-            if (el.ondrop == this.__dropFun) {
-                return;
-            }
-            el.ondrop = this.__dropFun;
-            el.ondragover = function() {
-                return false;
-            };
-        }
+        __dropFun : null
     },
 
     destruct : function() {
-        if (this.__selector != null && this.__selector.getContentElement() != null) {
-            var el = this.__selector.getContentElement().getDomElement();
-            el.ondrop = null;
-            el.ondragover = null;
-        }
         this.__selector = null;
         this.__editor = null;
     }
