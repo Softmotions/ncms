c3fc2b3d5a041269f411fb793ded13f3e3a75a5e
Антон Адаманский <adamansky@gmail.com>
2014-05-20 16:56:33 +0700

    files select dialog thumnail added

--- ncms-engine/ncms-engine-qx/src/main/qooxdoo/classes/ncms/mmgr/MediaSelectFileDlg.js
+++ ncms-engine/ncms-engine-qx/src/main/qooxdoo/classes/ncms/mmgr/MediaSelectFileDlg.js
@@ -31,19 +31,22 @@ qx.Class.define("ncms.mmgr.MediaSelectFileDlg", {
             showMaximize : true,
             allowMaximize : true,
             width : 620,
-            height : 400
+            height : 450
         });
 
         var vsp = new qx.ui.splitpane.Pane("horizontal");
         var folders = new ncms.mmgr.MediaItemTreeSelector(allowModify);
-        vsp.add(folders, {flex : 1});
 
+        var leftSide = new qx.ui.container.Composite(new qx.ui.layout.VBox(5));
+        leftSide.add(folders, {flex : 1});
+
+
+        var rightSide = new qx.ui.container.Composite(new qx.ui.layout.VBox());
         var files = new ncms.mmgr.MediaFilesSelector(allowModify);
         folders.bind("itemSelected", files, "item");
-        vsp.add(files, {flex : 3});
-        this.add(vsp, {flex : 1});
+        rightSide.add(files, {flex : 1});
 
-        //Botm buttons
+        //Bottom buttons
         var hcont = new qx.ui.container.Composite(new qx.ui.layout.HBox(5).set({"alignX" : "right"}));
         hcont.setPadding(5);
 
@@ -54,17 +57,38 @@ qx.Class.define("ncms.mmgr.MediaSelectFileDlg", {
         bt = new qx.ui.form.Button(this.tr("Cancel"));
         bt.addListener("execute", this.close, this);
         hcont.add(bt);
-        this.add(hcont);
+        rightSide.add(hcont);
+
+
+        var thumbnail = new qx.ui.basic.Image().set(
+                {allowGrowX : false, allowGrowY : false,
+                    allowShrinkX : false, allowShrinkY : false,
+                    alignX : "center",
+                    alignY : "middle",
+                    height : 128, width : 128,
+                    scale : true});
+
+        leftSide.add(thumbnail);
+        thumbnail.exclude();
+
+        vsp.add(leftSide, 1);
+        vsp.add(rightSide, 3);
+        this.add(vsp, {flex : 1});
 
         this.__closeCmd = new qx.ui.core.Command("Esc");
         this.__closeCmd.addListener("execute", this.close, this);
         this.addListenerOnce("resize", this.center, this);
 
-        //fixme qooxdoo bug workaround!
-        this.addListenerOnce("appear", function() {
-            folders.setLayoutProperties({ flex : 1 });
-            files.setLayoutProperties({ flex : 3 });
-        }, this);
+        files.addListener("fileSelected", function(ev) {
+            var spec = ev.getData();
+            var ctype = spec ? spec["content_type"] : null;
+            if (ncms.Utils.isImageContentType(ctype)) {
+                thumbnail.setSource(ncms.Application.ACT.getRestUrl("media.thumbnail2", spec));
+                thumbnail.show();
+            } else {
+                thumbnail.exclude();
+            }
+        });
     },
 
     members : {
