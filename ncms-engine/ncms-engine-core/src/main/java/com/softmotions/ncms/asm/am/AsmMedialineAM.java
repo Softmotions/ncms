7d6d768a7a3a0e475536702dc1f0b21cb5cc0530
Антон Адаманский <adamansky@gmail.com>
2014-09-11 12:57:15 +0700

    media gallery

--- ncms-engine/ncms-engine-core/src/main/java/com/softmotions/ncms/asm/am/AsmMedialineAM.java
+++ ncms-engine/ncms-engine-core/src/main/java/com/softmotions/ncms/asm/am/AsmMedialineAM.java
@@ -1,11 +1,14 @@
 package com.softmotions.ncms.asm.am;
 
+import com.softmotions.commons.json.JsonUtils;
 import com.softmotions.ncms.asm.Asm;
 import com.softmotions.ncms.asm.AsmAttribute;
+import com.softmotions.ncms.asm.AsmOptions;
 import com.softmotions.ncms.asm.render.AsmRendererContext;
 import com.softmotions.ncms.asm.render.AsmRenderingException;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,7 +22,7 @@ public class AsmMedialineAM implements AsmAttributeManager {
 
     private static final Logger log = LoggerFactory.getLogger(AsmMedialineAM.class);
 
-    private static final String[] TYPE = new String[] { "medialine" };
+    private static final String[] TYPE = new String[]{"medialine"};
 
     public String[] getSupportedAttributeTypes() {
         return TYPE;
@@ -34,7 +37,9 @@ public class AsmMedialineAM implements AsmAttributeManager {
     }
 
     public AsmAttribute applyAttributeOptions(AsmAttributeManagerContext ctx, AsmAttribute attr, JsonNode val) {
-        log.info("opts val=" + val);
+        AsmOptions asmOpts = new AsmOptions();
+        JsonUtils.populateMapByJsonNode((ObjectNode) val, asmOpts, "width");
+        attr.setOptions(asmOpts.toString());
         return attr;
     }
 
--- ncms-engine/ncms-engine-qx/src/main/qooxdoo/classes/ncms/asm/am/MedialineAM.js
+++ ncms-engine/ncms-engine-qx/src/main/qooxdoo/classes/ncms/asm/am/MedialineAM.js
@@ -20,13 +20,33 @@ qx.Class.define("ncms.asm.am.MedialineAM", {
 
     members : {
 
+        __form : null,
+
         activateOptionsWidget : function(attrSpec, asmSpec) {
-            return null;
+            var form = new qx.ui.form.Form();
+            var opts = ncms.Utils.parseOptions(attrSpec["options"]);
+            var el = new qx.ui.form.TextField();
+            el.setRequired(true);
+            if (opts["width"] != null) {
+                el.setValue(opts["width"]);
+            } else {
+                el.setValue("800");
+            }
+            form.add(el, this.tr("Max image width"), sm.util.Validate.canBeRangeNumber(10, 2048), "width");
+            var fr = new qx.ui.form.renderer.Single(form);
+            this.__form = form;
+            fr.setAllowGrowX(false);
+            return fr;
         },
 
         optionsAsJSON : function() {
-            var data = {};
-            return data;
+            if (this.__form == null || !this.__form.validate()) {
+                return null;
+            }
+            var items = this.__form.getItems();
+            return {
+                width : items["width"].getValue()
+            };
         },
 
         activateValueEditorWidget : function(attrSpec, asmSpec) {
@@ -44,8 +64,7 @@ qx.Class.define("ncms.asm.am.MedialineAM", {
         }
     },
 
-
     destruct : function() {
-        //this._disposeObjects("__field_name");                                
+        this._disposeObjects("__form");
     }
 });
