079a5e5d1f37fedfbea83c627592727c066b6da8
Tyutyunkov VE <tyutyunkov@gmail.com>
2014-10-15 16:38:02 +0700

    sorting

--- nsu-site/nsu-site-web/src/main/java/ru/nsu/SitemapController.java
+++ nsu-site/nsu-site-web/src/main/java/ru/nsu/SitemapController.java
@@ -2,11 +2,14 @@ package ru.nsu;
 
 import com.softmotions.ncms.asm.Asm;
 import com.softmotions.ncms.asm.AsmDAO;
+import com.softmotions.ncms.asm.CachedPage;
+import com.softmotions.ncms.asm.PageService;
 import com.softmotions.ncms.asm.render.AsmController;
 import com.softmotions.ncms.asm.render.AsmRendererContext;
 
 import com.google.inject.Inject;
 
+import org.apache.commons.lang3.ObjectUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,6 +18,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * @author Tyutyunkov Vyacheslav (tve@softmotions.com)
@@ -24,11 +29,13 @@ public class SitemapController implements AsmController {
     private static final Logger log = LoggerFactory.getLogger(SitemapController.class);
 
     private final AsmDAO adao;
+    private final PageService pageService;
 
     @Inject
 
-    public SitemapController(AsmDAO adao) {
+    public SitemapController(AsmDAO adao, PageService pageService) {
         this.adao = adao;
+        this.pageService = pageService;
     }
 
 
@@ -51,14 +58,31 @@ public class SitemapController implements AsmController {
             return true;
         }
 
-        ctx.put("layer", loadPageLayer(null));
+        // depends on current structure
+        CachedPage index = pageService.getIndexPage(req);
+
+        List<Asm> layer = loadPageLayer(null);
+        if (index != null) {
+            long indexId = index.getId();
+            Collections.sort(layer, (a1, a2) -> {
+                if (indexId == a1.getId()) {
+                    return -1;
+                } else if (indexId == a2.getId()) {
+                    return 1;
+                } else {
+                    return ObjectUtils.compare(a1.getHname(), a2.getHname());
+                }
+            });
+        }
+
+        ctx.put("layer", layer);
 
         return false;
     }
 
-    private Collection<Asm> loadPageLayer(Long parent) {
+    private List<Asm> loadPageLayer(Long parent) {
         Collection<Asm> asms = adao.selectPageLayer(parent);
-        Collection<Asm> result = new ArrayList<>(asms.size());
+        List<Asm> result = new ArrayList<>(asms.size());
         asms.stream().filter((asm) -> asm.isPublished() || "page.folder".equals(asm.getType())).forEach(result::add);
         return result;
     }
