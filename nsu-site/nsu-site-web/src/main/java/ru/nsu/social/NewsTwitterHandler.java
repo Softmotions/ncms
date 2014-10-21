05e67cf35c57245a797f87bc09cb1d78fb8557b0
Tyutyunkov VE <tyutyunkov@gmail.com>
2014-10-21 11:52:41 +0700

    news tweet

--- nsu-site/nsu-site-web/src/main/java/ru/nsu/social/NewsTwitterHandler.java
+++ nsu-site/nsu-site-web/src/main/java/ru/nsu/social/NewsTwitterHandler.java
@@ -36,7 +36,10 @@ import java.util.concurrent.Executors;
 @Singleton
 public class NewsTwitterHandler {
     private static final Logger log = LoggerFactory.getLogger(NewsTwitterHandler.class);
-    private static final String NEWS_TWEET_TYPE = "news_tweet";
+
+    private static final String NEWS_TWEET_REFDATA_TYPE = "news_tweet";
+    private static final String NEWS_PAGE_TYPE = "news.page";
+
     private static final int TWEET_MAX_LENGTH = 140;
 
     private final AsmDAO adao;
@@ -50,6 +53,8 @@ public class NewsTwitterHandler {
     private final ExecutorService tweetSender;
 
     private final String siteRoot;
+    private final String tweetAttr;
+    private final String annotationAttr;
 
 
     @Inject
@@ -64,6 +69,10 @@ public class NewsTwitterHandler {
         siteRoot = xcfg.getString("site-root");
 
         SubnodeConfiguration twCfg = xcfg.configurationAt("social.twitter");
+
+        tweetAttr = twCfg.getString("tweet-attribute", "tweet");
+        annotationAttr = twCfg.getString("annotation-attribute", "annotation");
+
         twProps.load(new StringReader(twCfg.getString("config-properties", "")));
 
         if (twProps.isEmpty()) {
@@ -94,11 +103,11 @@ public class NewsTwitterHandler {
         }
 
         tweetSender.execute(() -> {
-            Asm page = adao.selectOne("selectAsmByCriteria", "id", e.getId(), "type", "news.page");
-            if (page == null || !page.isPublished() || !BooleanUtils.toBoolean(page.getEffectiveAttributeAsString("tweet", "false"))) {
+            Asm page = adao.selectOne("selectAsmByCriteria", "id", e.getId(), "type", NEWS_PAGE_TYPE);
+            if (page == null || !page.isPublished() || !BooleanUtils.toBoolean(page.getEffectiveAttributeAsString(tweetAttr, "false"))) {
                 return;
             }
-            if (adao.count("countAsmRefByType", "id", page.getId(), "type", NEWS_TWEET_TYPE) > 0) {
+            if (adao.count("countAsmRefByType", "id", page.getId(), "type", NEWS_TWEET_REFDATA_TYPE) > 0) {
                 return;
             }
 
@@ -110,7 +119,7 @@ public class NewsTwitterHandler {
             String name = page.getHname();
             String parentName = parent.getHname() + ":";
             String link = siteRoot + pageService.resolvePageLink(page.getId());
-            String annotation = page.getEffectiveAttributeAsString("annotation", "");
+            String annotation = page.getEffectiveAttributeAsString(annotationAttr, "");
             if (StringUtils.isBlank(annotation)) {
                 annotation = name;
             }
@@ -139,7 +148,7 @@ public class NewsTwitterHandler {
                 try {
                     Twitter twitter = twFactory.getInstance();
                     twitter.updateStatus(text);
-                    adao.setAsmRefData(page.getId(), NEWS_TWEET_TYPE, "");
+                    adao.setAsmRefData(page.getId(), NEWS_TWEET_REFDATA_TYPE, "");
                 } catch (TwitterException ex) {
                     log.error("Error update twitter status: {}", ex.getMessage());
                 }
