9000bb44f26a59a75a5b9996c18eee53fe2b2bb4
Anton Adamansky <adamansky@gmail.com>
2016-07-10 14:14:14 +0600

    -  MttRulesRS

--- ncms-engine/ncms-engine-core/src/main/java/com/softmotions/ncms/marketing/mtt/MttRulesRS.kt
+++ ncms-engine/ncms-engine-core/src/main/java/com/softmotions/ncms/marketing/mtt/MttRulesRS.kt
@@ -19,69 +19,74 @@ open class MttRulesRS
 constructor(sess: SqlSession, val mapper: ObjectMapper) : MBDAOSupport(MttRulesRS::class.java, sess) {
 
     //todo use as virtual list
-    @GET
-    @Path("/list")
-    open fun rules(): ArrayNode = mapper.createArrayNode()
+//    @GET
+//    @Path("/select")
+//    open fun rules(): ArrayNode = mapper.createArrayNode()
+
+//      /rs/adm/mtt/rules/select
+//      /rs/adm/mtt/rules/select/count
 
 
 
-    @GET
-    @Path("/rule/{rid}")
-    open fun ruleGet(@PathParam("rid") rid: Long): ObjectNode = mapper.createObjectNode()
 
-    @PUT
-    @Path("/rule")
-    open fun ruleCreate(rule: ObjectNode): ObjectNode = mapper.createObjectNode()
 
-    @POST
-    @Path("/rule/{rid}")
-    open fun ruleUpdate(@PathParam("rid") rid: Long, rule: ObjectNode): ObjectNode = mapper.createObjectNode()
-
-    @DELETE
-    @Path("/rule/{rid}")
-    open fun ruleDelete(@PathParam("rid") rid: Long) = { }
-
-    @PUT
-    @Path("/rule/{rid}/filter")
-    open fun filterCreate(@PathParam("rid") rid: Long, filter: ObjectNode): ObjectNode = mapper.createObjectNode()
-
-    @GET
-    @Path("/rule/{id}/filters")
-    open fun filtersList(@PathParam("rid") rid: Long): ArrayNode = mapper.createArrayNode()
-
-    @GET
-    @Path("/filter/{fid}")
-    open fun filterGet(@PathParam("fid") fid: Long): ObjectNode = mapper.createObjectNode()
-
-    @POST
-    @Path("/filter/{fid}")
-    open fun filterUpdate(@PathParam("fid") fid: Long, filter: ObjectNode): ObjectNode = mapper.createObjectNode()
-
-    @DELETE
-    @Path("/filter/{fid}")
-    open fun filterDelete(@PathParam("fid") fid: Long) = { }
-
-    @GET
-    @Path("/rule/{rid}/actions")
-    open fun actionsList(@PathParam("rid") rid: Long): ArrayNode = mapper.createArrayNode()
-
-    @PUT
-    @Path("/rule/{rid}/action")
-    open fun actionCreate(@PathParam("rid") rid: Long, action: ObjectNode): ObjectNode = mapper.createObjectNode()
-
-    @GET
-    @Path("/action/{aid}")
-    open fun actionGet(@PathParam("aid") aid: Long): ObjectNode = mapper.createObjectNode()
-
-    @POST
-    @Path("/action/{aid}")
-    open fun actionUpdate(@PathParam("aid") aid: Long, action: ObjectNode): ObjectNode = mapper.createObjectNode()
-
-    @DELETE
-    @Path("/action/{aid}")
-    open fun actionDelete(@PathParam("aid") aid: Long) = {}
-
-    @POST
-    @Path("/action/{aid}/move")
-    open fun actionMove(@PathParam("aid") aid: Long, spec: ObjectNode): ArrayNode = mapper.createArrayNode()
+//    @GET
+//    @Path("/rule/{rid}")
+//    open fun ruleGet(@PathParam("rid") rid: Long): ObjectNode = mapper.createObjectNode()
+//
+//    @PUT
+//    @Path("/rule")
+//    open fun ruleCreate(rule: ObjectNode): ObjectNode = mapper.createObjectNode()
+//
+//    @POST
+//    @Path("/rule/{rid}")
+//    open fun ruleUpdate(@PathParam("rid") rid: Long, rule: ObjectNode): ObjectNode = mapper.createObjectNode()
+//
+//    @DELETE
+//    @Path("/rule/{rid}")
+//    open fun ruleDelete(@PathParam("rid") rid: Long) = { }
+//
+//    @PUT
+//    @Path("/rule/{rid}/filter")
+//    open fun filterCreate(@PathParam("rid") rid: Long, filter: ObjectNode): ObjectNode = mapper.createObjectNode()
+//
+//    @GET
+//    @Path("/rule/{id}/filters")
+//    open fun filtersList(@PathParam("rid") rid: Long): ArrayNode = mapper.createArrayNode()
+//
+//    @GET
+//    @Path("/filter/{fid}")
+//    open fun filterGet(@PathParam("fid") fid: Long): ObjectNode = mapper.createObjectNode()
+//
+//    @POST
+//    @Path("/filter/{fid}")
+//    open fun filterUpdate(@PathParam("fid") fid: Long, filter: ObjectNode): ObjectNode = mapper.createObjectNode()
+//
+//    @DELETE
+//    @Path("/filter/{fid}")
+//    open fun filterDelete(@PathParam("fid") fid: Long) = { }
+//
+//    @GET
+//    @Path("/rule/{rid}/actions")
+//    open fun actionsList(@PathParam("rid") rid: Long): ArrayNode = mapper.createArrayNode()
+//
+//    @PUT
+//    @Path("/rule/{rid}/action")
+//    open fun actionCreate(@PathParam("rid") rid: Long, action: ObjectNode): ObjectNode = mapper.createObjectNode()
+//
+//    @GET
+//    @Path("/action/{aid}")
+//    open fun actionGet(@PathParam("aid") aid: Long): ObjectNode = mapper.createObjectNode()
+//
+//    @POST
+//    @Path("/action/{aid}")
+//    open fun actionUpdate(@PathParam("aid") aid: Long, action: ObjectNode): ObjectNode = mapper.createObjectNode()
+//
+//    @DELETE
+//    @Path("/action/{aid}")
+//    open fun actionDelete(@PathParam("aid") aid: Long) = {}
+//
+//    @POST
+//    @Path("/action/{aid}/move")
+//    open fun actionMove(@PathParam("aid") aid: Long, spec: ObjectNode): ArrayNode = mapper.createArrayNode()
 }
