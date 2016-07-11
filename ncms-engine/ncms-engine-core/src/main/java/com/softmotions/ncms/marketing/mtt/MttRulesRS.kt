71a286aaf3831ee82d958e53c47e3a39bdaa88cc
Tyutyunkov Vyacheslav <tve@softmotions.com>
2016-07-11 23:23:01 +0600

    reformat

--- ncms-engine/ncms-engine-core/src/main/java/com/softmotions/ncms/marketing/mtt/MttRulesRS.kt
+++ ncms-engine/ncms-engine-core/src/main/java/com/softmotions/ncms/marketing/mtt/MttRulesRS.kt
@@ -34,8 +34,8 @@ constructor(val sess: SqlSession,
     @GET
     @Path("/select")
     @Transactional
-    open fun rules(@Context req: HttpServletRequest): Response = Response
-            .ok(StreamingOutput { output ->
+    open fun rules(@Context req: HttpServletRequest): Response =
+            Response.ok(StreamingOutput { output ->
                 with(mapper.factory.createGenerator(output)) {
                     writeStartArray()
                     selectByCriteria(createRulesQ(req), { context ->
@@ -46,14 +46,15 @@ constructor(val sess: SqlSession,
                     flush()
                 }
             })
-            .type("application/json;charset=UTF-8")
-            .build()
+                    .type("application/json;charset=UTF-8")
+                    .build()
 
     @GET
     @Path("/select/count")
     @Produces("text/plain")
     @Transactional
-    open fun rulesCount(@Context req: HttpServletRequest): Int = selectOneByCriteria(createRulesQ(req).withStatement("selectRulesCount"))
+    open fun rulesCount(@Context req: HttpServletRequest): Int =
+            selectOneByCriteria(createRulesQ(req).withStatement("selectRulesCount"))
 
     private fun createRulesQ(req: HttpServletRequest): MBCriteriaQuery<out MBCriteriaQuery<*>> {
         val cq = createCriteria()
@@ -83,7 +84,8 @@ constructor(val sess: SqlSession,
     @GET
     @Path("/rule/{rid}")
     @Transactional
-    open fun ruleGet(@PathParam("rid") rid: Long): MttRule = selectOne("selectRuleById", rid) ?: throw NotFoundException()
+    open fun ruleGet(@PathParam("rid") rid: Long): MttRule =
+            selectOne("selectRuleById", rid) ?: throw NotFoundException()
 
     @PUT
     @Path("/rule/{name}")
@@ -109,16 +111,17 @@ constructor(val sess: SqlSession,
     @Transactional
     open fun ruleRename(@Context req: HttpServletRequest,
                         @PathParam("rid") rid: Long,
-                        @PathParam("name") name: String): MttRule = synchronized(MttRule::class) {
-        val rname = name.trim();
-        if (selectOne<Long?>("selectRuleIdByName", rname) != null) {
-            throw NcmsMessageException(messages.get("ncms.mtt.rule.name.already.other", req, rname), true)
-        }
+                        @PathParam("name") name: String): MttRule =
+            synchronized(MttRule::class) {
+                val rname = name.trim();
+                if (selectOne<Long?>("selectRuleIdByName", rname) != null) {
+                    throw NcmsMessageException(messages.get("ncms.mtt.rule.name.already.other", req, rname), true)
+                }
 
-        update("updateRuleName", "id", rid, "name", rname);
+                update("updateRuleName", "id", rid, "name", rname);
 
-        return ruleGet(rid);
-    }
+                return ruleGet(rid);
+            }
 
     @POST
     @Path("/rule/{rid}/flags/{flags}")
@@ -134,7 +137,8 @@ constructor(val sess: SqlSession,
     @Path("/rule/{rid}/move/{direction}")
     open fun ruleMove(@PathParam("rid") rid: Long, @PathParam("direction") direction: Byte) {
         val rule = ruleGet(rid)
-        val sordinal = selectOne<Long?>(if (direction < 0) "selectPreviousRule" else "selectNextRule", rule.ordinal) ?: return
+        val sordinal =
+                selectOne<Long?>(if (direction < 0) "selectPreviousRule" else "selectNextRule", rule.ordinal) ?: return
         update("updateRuleOrdinal", "ordinal", sordinal, "newOrdinal", rule.ordinal)
         update("updateRuleOrdinal", "id", rule.id, "newOrdinal", sordinal)
     }
@@ -143,7 +147,8 @@ constructor(val sess: SqlSession,
     @Path("/rule/{rid}")
     @Transactional
     // TODO: events?
-    open fun ruleDelete(@PathParam("rid") rid: Long) = delete("deleteRuleById", rid)
+    open fun ruleDelete(@PathParam("rid") rid: Long) =
+            delete("deleteRuleById", rid)
 
     // TODO: rule enabling/disabling
 
@@ -172,8 +177,8 @@ constructor(val sess: SqlSession,
     @Path("/rule/{rid}/filters/select/count")
     @Produces("text/plain")
     @Transactional
-    open fun filtersCount(@Context req: HttpServletRequest,
-                          @PathParam("rid") rid: Long): Long = selectOneByCriteria(createFiltersQ(rid, req).withStatement("selectFiltersCount"))
+    open fun filtersCount(@Context req: HttpServletRequest, @PathParam("rid") rid: Long): Long =
+            selectOneByCriteria(createFiltersQ(rid, req).withStatement("selectFiltersCount"))
 
     private fun createFiltersQ(rid: Long, req: HttpServletRequest): MBCriteriaQuery<out MBCriteriaQuery<*>> {
         val cq = createCriteria()
@@ -196,7 +201,8 @@ constructor(val sess: SqlSession,
     @GET
     @Path("/filter/{fid}")
     @Transactional
-    open fun filterGet(@PathParam("fid") fid: Long): MttRuleFilter = selectOne("selectFilterById", fid) ?: throw NotFoundException()
+    open fun filterGet(@PathParam("fid") fid: Long): MttRuleFilter =
+            selectOne("selectFilterById", fid) ?: throw NotFoundException()
 
     @PUT
     @Path("/rule/{rid}/filter/{type}")
@@ -225,7 +231,8 @@ constructor(val sess: SqlSession,
     @DELETE
     @Path("/filter/{fid}")
     @Transactional
-    open fun filterDelete(@PathParam("fid") fid: Long) = delete("deleteFilterById", fid)
+    open fun filterDelete(@PathParam("fid") fid: Long) =
+            delete("deleteFilterById", fid)
 
 //    @GET
 //    @Path("/rule/{rid}/actions")
