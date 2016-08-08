b3802b09172fb4899aa5e2d2340eb5a8899a6b54
Anton Adamansky <adamansky@gmail.com>
2016-08-08 23:59:34 +0700

    - docs

--- ncms-docs/ncms-docs-ru/conf.py
+++ ncms-docs/ncms-docs-ru/conf.py
@@ -28,6 +28,8 @@ rst_epilog = """
 .. |ncmsversion| replace:: nCMS 1.0
 """
 
+highlight_language = 'text'
+
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
