2f77a087a6e0d710062c745326c52db1fea30807
Anton Adamansky <adamansky@gmail.com>
2016-08-08 13:09:11 +0700

    - new ace editor integrated

--- ncms-engine/ncms-engine-qx/src/main/qooxdoo/classes/ncms/mmgr/MediaTextFileEditor.js
+++ ncms-engine/ncms-engine-qx/src/main/qooxdoo/classes/ncms/mmgr/MediaTextFileEditor.js
@@ -42,17 +42,17 @@ qx.Class.define("ncms.mmgr.MediaTextFileEditor", {
             badIE = parseFloat(qx.core.Environment.get("browser.version")) <= 8 ||
                 qx.core.Environment.get("browser.documentmode") <= 8;
         }
-        var canAce = !!(!document.createElement("div").getBoundingClientRect || badIE || !window.ace);
-
-        console.log("canAce=" + canAce);
-        if (true) {
+        var canAce = !(!document.createElement("div").getBoundingClientRect || badIE || !window.ace);
+        if (!canAce) {
             this.__area = new qx.ui.form.TextArea().set({font: "monospace", wrap: false});
             this._add(this.__area, {flex: 1});
             this.__area.addListener("input", function () {
                 this.__broadcaster.setEnabled(true);
             }, this);
         } else { // setup ace editor
-
+            this.__aceContainer = new qx.ui.core.Widget();
+            this.__aceContainer.addListenerOnce("appear", this.__onAceContainerAppear, this);
+            this._add(this.__aceContainer, {flex: 1});
         }
 
         var hcont = new qx.ui.container.Composite(new qx.ui.layout.HBox(5));
@@ -81,13 +81,23 @@ qx.Class.define("ncms.mmgr.MediaTextFileEditor", {
          */
         __ace: null,
 
+        __pendigCode: null,
+
+        __pendigRo: null,
+
+        __readOnly: false,
+
         __broadcaster: null,
 
         setReadOnly: function (state) {
+            this.__readOnly = !!state;
             if (this.__area) {
                 this.__area.setReadOnly(state);
+            } else if (this.__ace) {
+                this.__ace.setReadOnly(state);
+            } else {
+                this.__pendigRo = state
             }
-
             this.__broadcaster.setEnabled(false);
         },
 
@@ -143,16 +153,73 @@ qx.Class.define("ncms.mmgr.MediaTextFileEditor", {
         },
 
 
-        __getCode: function() {
-            return this.__area.getValue();
+        __getCode: function () {
+            if (this.__area) {
+                return this.__area.getValue();
+            } else if (this.__ace) {
+                return this.__ace.getSession().getValue();
+            }
         },
 
         __setCode: function (code, lclass) {
-            this.__area.setValue(code);
+            if (this.__area) {
+                this.__area.setValue(code);
+            } else if (this.__ace) {
+                var ace = this.__ace;
+                var sess = ace.getSession();
+                if (lclass != null) {
+                    var Mode = require("ace/mode/" + lclass).Mode;
+                    sess.setMode(new Mode());
+                }
+                sess.setValue(code);
+                // move cursor to start to prevent scrolling to the bottom
+                ace.renderer.scrollToX(0);
+                ace.renderer.scrollToY(0);
+                ace.selection.moveCursorFileStart();
+            } else {
+                this.__pendigCode = {code: code, lclass: lclass};
+            }
+            this.__broadcaster.setEnabled(false);
         },
 
         __cleanup: function () {
-            this.__area.resetValue();
+            if (this.__area) {
+                this.__area.resetValue();
+            } else if (this.__ace) {
+                //todo review
+                this.__setCode("");
+            }
+        },
+
+        __onAceContainerAppear: function () {
+            // timout needed for chrome to not get the ACE layout wrong and show the
+            // text on top of the gutter
+            qx.event.Timer.once(function () {
+                var me = this;
+                var ace = this.__ace = window.ace.edit(this.__aceContainer.getContentElement().getDomElement());
+                var session = ace.getSession();
+                session.setUseSoftTabs(true);
+                session.setTabSize(2);
+                ace.renderer.setShowGutter(false);
+                //ace.renderer.setOption("showLineNumbers", false);
+                if (this.__pendigCode != null) {
+                    this.__setCode(this.__pendigCode["code"], this.__pendigCode["lclass"]);
+                    this.__pendigCode = null;
+                }
+                if (this.__pendigRo != null) {
+                    ace.setReadOnly(this.__pendigRo);
+                    this.__pendigRo = null;
+                }
+                session.on("change", function () {
+                    me.__broadcaster.setEnabled(true);
+                });
+                this.__aceContainer.addListener("resize", function () {
+                    // use a timeout to let the layout queue apply its changes to the dom
+                    window.setTimeout(function () {
+                        me.__ace.resize();
+                    }, 0);
+                });
+            }, this, 500);
         }
     },
 
@@ -160,6 +227,8 @@ qx.Class.define("ncms.mmgr.MediaTextFileEditor", {
         this.__aceContainer = null;
         this.__ace = null;
         this.__area = null;
+        this.__pendigCode = null;
+        this.__pendigRo = null;
         this.__broadcaster.destruct();
         //this._disposeObjects("__field_name");
     }
