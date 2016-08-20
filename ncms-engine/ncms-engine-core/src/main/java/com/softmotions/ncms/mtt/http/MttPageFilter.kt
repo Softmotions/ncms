d485b1c9d8b48380e48c40f25a9ac9ea9f2f0546
Павел Мотырев <legioner.r@gmail.com>
2016-08-20 15:02:48 +0700

    #347 PageFilter

--- ncms-engine/ncms-engine-core/src/main/java/com/softmotions/ncms/mtt/http/MttPageFilter.kt
+++ ncms-engine/ncms-engine-core/src/main/java/com/softmotions/ncms/mtt/http/MttPageFilter.kt
@@ -27,21 +27,26 @@ constructor(private val ps: PageService) : MttFilterHandler {
 
     private val GUID_REGEXP: Regex = Regex("[0-9a-f]{32}")
 
-    private val LEADING_SLASH_REGEXP: Regex = Regex("^/")
-
     override fun matched(ctx: MttFilterHandlerContext, req: HttpServletRequest): Boolean {
         val spec = ctx.spec
-        var pageName = GUID_REGEXP.find(spec.path("pageref").asText(""))?.value;
-        if (pageName.isNullOrBlank()) {
-            return false
+        var pageName = GUID_REGEXP.find(spec.path("pageref").asText(null))?.value ?: return false
+
+        var uri = req.requestURI
+        if (uri[0] == '/') {
+            uri = uri.substring(1)
         }
-        var uri = LEADING_SLASH_REGEXP.replace(req.requestURI, "");
 
         if (log.isDebugEnabled) {
             log.info("match='$pageName' req='${req.requestURI}'")
         }
 
-        var cp: CachedPage? = ps.getCachedPage(uri, true);
+        var cp = ps.getCachedPage(uri, true)
+        if (cp == null) {
+            if (uri.isNotBlank() && !"index.html".equals(uri)) {
+                return false
+            }
+            cp = ps.getIndexPage(req, true)
+        }
         return (cp != null) && (pageName.equals(cp.name))
     }
 }
