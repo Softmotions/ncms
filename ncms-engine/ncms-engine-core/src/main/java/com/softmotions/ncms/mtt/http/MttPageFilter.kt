cf47b008b977e357b6b734307ae60aa87ba8f26f
Павел Мотырев <legioner.r@gmail.com>
2016-08-20 13:30:37 +0700

    #347 PageFilter

--- ncms-engine/ncms-engine-core/src/main/java/com/softmotions/ncms/mtt/http/MttPageFilter.kt
+++ ncms-engine/ncms-engine-core/src/main/java/com/softmotions/ncms/mtt/http/MttPageFilter.kt
@@ -1,6 +1,9 @@
 package com.softmotions.ncms.mtt.http
 
+import com.google.inject.Inject
 import com.google.inject.Singleton
+import com.softmotions.ncms.asm.CachedPage
+import com.softmotions.ncms.asm.PageService
 import org.slf4j.LoggerFactory
 import javax.annotation.concurrent.ThreadSafe
 import javax.servlet.http.HttpServletRequest
@@ -12,7 +15,9 @@ import javax.servlet.http.HttpServletRequest
  */
 @Singleton
 @ThreadSafe
-open class MttPageFilter : MttFilterHandler {
+open class MttPageFilter
+@Inject
+constructor(private val ps: PageService) : MttFilterHandler {
 
     companion object {
         val log = LoggerFactory.getLogger(MttPageFilter::class.java)
@@ -20,18 +25,23 @@ open class MttPageFilter : MttFilterHandler {
 
     override val type: String = "page"
 
+    private val GUID_REGEXP: Regex = Regex("[0-9a-f]{32}")
+
+    private val LEADING_SLASH_REGEXP: Regex = Regex("^/")
+
     override fun matched(ctx: MttFilterHandlerContext, req: HttpServletRequest): Boolean {
         val spec = ctx.spec
-        var pageName = Regex("[0-9a-f]{32}").find(spec.path("pageref").asText())?.value;
-        var uri = Regex("^/").replace(req.requestURI, "");
+        var pageName = GUID_REGEXP.find(spec.path("pageref").asText(""))?.value;
+        if (pageName.isNullOrBlank()) {
+            return false
+        }
+        var uri = LEADING_SLASH_REGEXP.replace(req.requestURI, "");
 
         if (log.isDebugEnabled) {
             log.info("match='$pageName' req='${req.requestURI}'")
         }
 
-        // todo: get NAME from ASMS with NAME or NAV_ALIAS == uri
-        // todo: return NAME == pageName
-        log.info("pageName='$pageName' uri='$uri'")
-        return uri == pageName // TODO FIXME
+        var cp: CachedPage? = ps.getCachedPage(uri, true);
+        return (cp != null) && (pageName.equals(cp.name))
     }
 }
