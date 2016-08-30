011cd104d08a05241a7456f37936f71f74e0f455
Anton Adamansky <adamansky@gmail.com>
2016-08-30 15:08:52 +0700

    - #20

---
+++ ncms-engine/ncms-engine-tests/src/test/java/com/softmotions/ncms/db/_TestAsmRSDB.kt
@@ -0,0 +1,44 @@
+package com.softmotions.ncms.db
+
+import com.softmotions.ncms.DbBaseTest
+import com.softmotions.ncms.asm.AsmRS
+import com.softmotions.weboot.mb.MBDAOSupport
+import org.apache.ibatis.session.SqlSession
+import org.testng.annotations.AfterClass
+import org.testng.annotations.BeforeClass
+import org.testng.annotations.Test
+
+/**
+ * Testing of sql queries for AsmRS
+ *
+ * @author Adamansky Anton (adamansky@gmail.com)
+ */
+class _TestAsmRSDB(db: String) : DbBaseTest(db) {
+
+    constructor() : this(DEFAULT_DB) {
+    }
+
+    val ds: MBDAOSupport by lazy {
+        MBDAOSupport(AsmRS::class.java, getInstance(SqlSession::class))
+    }
+
+    @BeforeClass
+    fun setup() {
+        super.setup("com/softmotions/ncms/db/cfg/test-ncms-db-conf.xml")
+    }
+
+    @AfterClass
+    override fun shutdown() {
+        super.shutdown()
+    }
+
+    // todo write AsmRS.xml sql test here
+
+    @Test
+    fun testSelect() {
+        //todo
+        //ds.select<Map<String,Any>>("select")
+    }
+
+
+}
