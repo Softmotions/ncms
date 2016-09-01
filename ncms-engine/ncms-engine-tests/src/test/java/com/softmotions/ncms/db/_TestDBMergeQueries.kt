1394e257cfa167cdffd241ad22ad0455b4e4acdc
Павел Мотырев <legioner.r@gmail.com>
2016-09-01 19:11:52 +0700

    #366 merge queries

--- ncms-engine/ncms-engine-tests/src/test/java/com/softmotions/ncms/db/_TestDBMergeQueries.kt
+++ ncms-engine/ncms-engine-tests/src/test/java/com/softmotions/ncms/db/_TestDBMergeQueries.kt
@@ -8,6 +8,7 @@ import com.softmotions.ncms.rds.RefDataStore
 import com.softmotions.ncms.user.UserEnvRS
 import com.softmotions.weboot.mb.MBDAOSupport
 import org.apache.ibatis.session.SqlSession
+import org.testng.Assert
 import org.testng.annotations.AfterClass
 import org.testng.annotations.BeforeClass
 import org.testng.annotations.Test
@@ -71,15 +72,17 @@ class _TestDBMergeQueries(db: String) : DbBaseTest(db) {
         adao.asmInsert(asm)
 
         // test query upsertAttirbute@AsmDAO
-        adao.update("upsertAttribute",
-                "asmId" , 1L,
-                "name" , "test",
-                "label" , "test",
-                "type" , "test",
-                "options" , "test",
-                "required" , false,
-                "value" , "test",
-                "largeValue" , "test")
+        for (i in 0..1) {
+            Assert.assertEquals(1, adao.update("upsertAttribute",
+                    "asmId", 1L,
+                    "name", "test",
+                    "label", "test",
+                    "type", "test",
+                    "options", "test",
+                    "required", false,
+                    "value", "test",
+                    "largeValue", "test"))
+        }
 
         // test query mergeFileDependencies@AsmAttributeManagerContext
         mediaRS.insert("insertEntity",
@@ -94,67 +97,104 @@ class _TestDBMergeQueries(db: String) : DbBaseTest(db) {
                 "tags" , "test",
                 "meta" , "test",
                 "system" , false)
-        amContext.update("mergeFileDependencies", "list", listOf(listOf(1L, 1L)))
+        for (i in 0..1) {
+            val res = when (i) {
+                0 -> 1
+                1 -> 0
+                else -> 0
+            }
+            Assert.assertEquals(res, amContext.update("mergeFileDependencies",
+                    "list", listOf(listOf(1L, 1L))))
+        }
 
         // test query mergePageDependencies@AsmAttributeManagerContext
-        // todo: rewrite "and exists"
-//        amContext.update("mergePageDependencies", "list", listOf(listOf(1L, 1L)))
+        for (i in 0..1) {
+            val res = when (i) {
+                0 -> 1
+                1 -> 0
+                else -> 0
+            }
+            Assert.assertEquals(res, amContext.update("mergePageDependencies",
+                    "list", listOf(listOf(1L, "foo"))))
+        }
+        Assert.assertEquals(0, amContext.update("mergePageDependencies",
+                "list", listOf(listOf(1L, "bar"))))
 
         // test query setAsmRefData@AsmDAO
-        // todo: add unique constraints?
-/*
-        adao.update("setAsmRefData",
-                "id", 1L,
-                "type", "test",
-                "svalue", "test",
-                "ivalue", 1L)
-*/
+        for (i in 0..1) {
+            val res = when (i) {
+                0 -> 1
+                1 -> 0
+                else -> 0
+            }
+            Assert.assertEquals(res, adao.update("setAsmRefData",
+                    "id", 1L,
+                    "type", "test",
+                    "svalue", "test",
+                    "ivalue", 1L))
+        }
 
         // test query mergeNewPage@PageRS
-        pageRs.update("mergeNewPage",
-                "guid" , "test",
-                "name" , "test",
-                "type" , "test",
-                "nav_parent_id" , 1L,
-                "lang" , "RU",
-                "nav_cached_path" , "test",
-                "options" , "",
-                "user" , "test",
-                "description" , "test",
-                "recursive_acl_id" , 1L)
+        for (i in 0..1) {
+            Assert.assertEquals(1, pageRs.update("mergeNewPage",
+                    "guid", "test",
+                    "name", "test",
+                    "type", "test",
+                    "nav_parent_id", 1L,
+                    "lang", "RU",
+                    "nav_cached_path", "test",
+                    "options", "",
+                    "user", "test",
+                    "description", "test",
+                    "recursive_acl_id", 1L))
+        }
 
         // test query updateAclUserRights@PageSecurityService
-        // todo: add unique constraints?
+        //todo: fixme - different behavior on DB2 (1, 1) and PostgreSQL (1, 0)
 /*
-        pageSec.update("updateAclUserRights",
-                "acl", 1L,
-                "user", "test",
-                "rights", "test")
+        for (i in 0..1) {
+            val res = when (i) {
+                0 -> 1
+                1 -> 0
+                else -> 0
+            }
+            Assert.assertEquals(res, pageSec.update("updateAclUserRights",
+                    "acl", 1L,
+                    "user", "test",
+                    "rights", "test"))
+        }
 */
 
         // test query updateChildRecursiveAcl2@PageSecurityService
-        // todo: add unique constraints?
-/*        pageSec.update("updateChildRecursiveAcl2",
-                "acl", 1L,
-                "user", "test",
-                "rights", "test",
-                "nav_path", "test")
-*/
+        for (i in 0..1) {
+            Assert.assertEquals(0, pageSec.update("updateChildRecursiveAcl2",
+                    "acl", 1L,
+                    "user", "test",
+                    "rights", "test",
+                    "nav_path", "test"))
+        }
 
         // test query saveData@RefDataStore
-        refDS.update("saveData",
+        for (i in 0..1) {
+            Assert.assertEquals(1, refDS.update("saveData",
                 "ref", "test",
                 "data", null,
-                "content_type", "test")
+                "content_type", "test"))
+        }
 
         // test query addSet@UserEnvRS
-        // todo: add unique constraints?
-/*        userER.update("addSet",
-                "userid", "test",
-                "type", "test",
-                "vcol", "svalue",
-                "value", "test")
-*/
+        for (i in 0..1) {
+            val res = when (i) {
+                0 -> 1
+                1 -> 0
+                else -> 0
+            }
+            Assert.assertEquals(res, userER.update("addSet",
+                    "userid", "test",
+                    "type", "test",
+                    "vcol", "svalue",
+                    "value", "test"))
+        }
 
     }
 
