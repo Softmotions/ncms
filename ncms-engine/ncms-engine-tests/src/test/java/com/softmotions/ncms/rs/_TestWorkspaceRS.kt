e43ee85a1620d25ce7638641a801c5d393834e1f
Anton Adamansky <adamansky@gmail.com>
2016-09-02 12:18:58 +0700

    test cases refactoring

---
+++ ncms-engine/ncms-engine-tests/src/test/java/com/softmotions/ncms/rs/_TestWorkspaceRS.kt
@@ -0,0 +1,39 @@
+package com.softmotions.ncms.rs
+
+import org.testng.Assert.*
+import org.testng.annotations.AfterClass
+import org.testng.annotations.BeforeClass
+import org.testng.annotations.Test
+
+
+/**
+ * @author Adamansky Anton (adamansky@gmail.com)
+ */
+@Test(groups = arrayOf("rs"))
+class _TestWorkspaceRS(db: String) : BaseRSTest(db) {
+
+    constructor() : this(DEFAULT_DB) {
+    }
+
+    @BeforeClass
+    override fun setup() {
+        super.setup()
+    }
+
+    @AfterClass
+    override fun shutdown() {
+        super.shutdown()
+    }
+
+    @Test
+    fun testWorkspaceRS() {
+        with(GET("/rs/adm/ws/state")) {
+            assertEquals(code(), 200)
+            //todo
+            log.info("Body={}", body())
+        }
+
+        val env = getEnv()
+        log.info("env={}", env)
+    }
+}
