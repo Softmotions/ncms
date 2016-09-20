1248a19a0c8f3d89ed24db3ef6202deff7ec4954
Михаил Остроушенко <ostroushenko@gmail.com>
2016-09-20 13:31:16 +0700

    387ordering

--- ncms-engine/ncms-engine-core/src/main/java/com/softmotions/ncms/mtt/MttRulesRS.kt
+++ ncms-engine/ncms-engine-core/src/main/java/com/softmotions/ncms/mtt/MttRulesRS.kt
@@ -107,6 +107,7 @@ constructor(val sess: SqlSession,
             val rule = MttRule(rname)
             insert("insertRule", rule)
             val rid = selectOne<Long?>("selectRuleIdByName", rname) ?: throw InternalServerErrorException()
+            ruleSetFirst(rid)
             ebus.fireOnSuccessCommit(MttRuleUpdatedEvent(rid))
             return ruleGet(rid)
         }
@@ -173,6 +174,12 @@ constructor(val sess: SqlSession,
                 "ordinal2", rule.ordinal)
 
         ebus.fireOnSuccessCommit(MttRuleReorderedEvent(sordinal, rule.ordinal))
+    }
+
+    /***********/
+    private fun ruleSetFirst(@PathParam("rid") rid:Long) {
+        update("setRuleFirst",
+                "id", rid)
     }
 
     @DELETE
