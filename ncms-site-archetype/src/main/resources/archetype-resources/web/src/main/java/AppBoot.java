dfde10c3d3655349de1c6fb03bb7a27d4e2b7ccf
Anton Adamansky <adamansky@gmail.com>
2016-12-07 18:25:33 +0700

    - ncms-site-archetype fixes

---
+++ ncms-site-archetype/src/main/resources/archetype-resources/web/src/main/java/AppBoot.java
@@ -0,0 +1,9 @@
+package ${package};
+
+import com.softmotions.ncms.NcmsBoot;
+
+/**
+ * Applicaton starting point.
+ */
+public class AppBoot extends NcmsBoot {
+}
--- ncms-site-archetype/src/main/resources/archetype-resources/web/src/main/java/AppServletListener.java
+++
@@ -1,9 +0,0 @@
-package ${package};
-
-import com.softmotions.ncms.NcmsServletListener;
-
-/**
- * Applicaton starting point.
- */
-public class AppServletListener extends NcmsServletListener {
-}
