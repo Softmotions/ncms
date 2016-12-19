5b28ca63800cf5e7e61fe01cee60d7f14bf770f0
Anton Adamansky <adamansky@gmail.com>
2016-12-19 21:25:43 +0700

    - #50

---
+++ ncms-extensions/ncms-js-converter/src/main/java/com/softmotions/ncms/js/ClosureLoggerErrorManager.java
@@ -0,0 +1,67 @@
+package com.softmotions.ncms.js;
+
+
+import org.slf4j.Logger;
+
+import com.google.javascript.jscomp.BasicErrorManager;
+import com.google.javascript.jscomp.CheckLevel;
+import com.google.javascript.jscomp.JSError;
+import com.google.javascript.jscomp.LightweightMessageFormatter;
+import com.google.javascript.jscomp.MessageFormatter;
+import com.google.javascript.jscomp.parsing.parser.util.format.SimpleFormat;
+
+/**
+ * Based on {@link com.google.javascript.jscomp.LoggerErrorManager}
+ *
+ * @author Adamansky Anton (adamansky@gmail.com)
+ */
+public class ClosureLoggerErrorManager extends BasicErrorManager {
+    private final MessageFormatter formatter;
+    private final Logger logger;
+
+    /**
+     * Creates an instance.
+     */
+    public ClosureLoggerErrorManager(MessageFormatter formatter, Logger logger) {
+        this.formatter = formatter;
+        this.logger = logger;
+    }
+
+    /**
+     * Creates an instance with a source-less error formatter.
+     */
+    public ClosureLoggerErrorManager(Logger logger) {
+        this(LightweightMessageFormatter.withoutSource(), logger);
+    }
+
+    @Override
+    public void println(CheckLevel level, JSError error) {
+        switch (level) {
+            case ERROR:
+                logger.error(error.format(level, formatter));
+                break;
+            case WARNING:
+                logger.warn(error.format(level, formatter));
+                break;
+            case OFF:
+                break;
+        }
+    }
+
+    @Override
+    protected void printSummary() {
+        String msg = null;
+        if (getTypedPercent() > 0.0) {
+            msg = SimpleFormat.format(
+                    "%d error(s), %d warning(s), %.1f%% typed",
+                    getErrorCount(), getWarningCount(), getTypedPercent());
+        } else if (getErrorCount() + getWarningCount() > 0) {
+            msg = SimpleFormat.format(
+                    "%d error(s), %d warning(s)",
+                    getErrorCount(), getWarningCount());
+        }
+        if (msg != null) {
+            logger.warn(msg);
+        }
+    }
+}
--- ncms-extensions/ncms-js-converter/src/main/java/com/softmotions/ncms/js/JsRS.java
+++ ncms-extensions/ncms-js-converter/src/main/java/com/softmotions/ncms/js/JsRS.java
@@ -1,26 +1,40 @@
 package com.softmotions.ncms.js;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import javax.annotation.Nullable;
 import javax.ws.rs.BadRequestException;
 import javax.ws.rs.NotFoundException;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.StreamingOutput;
+import javax.ws.rs.Produces;
 
-import org.apache.commons.io.IOUtils;
+import org.apache.commons.io.FileUtils;
 import org.apache.ibatis.session.SqlSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import com.google.common.util.concurrent.Striped;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import com.google.javascript.jscomp.CommandLineRunner;
+import com.google.javascript.jscomp.Compiler;
+import com.google.javascript.jscomp.CompilerOptions;
+import com.google.javascript.jscomp.CompilerOptions.LanguageMode;
+import com.google.javascript.jscomp.Result;
+import com.google.javascript.jscomp.SourceFile;
+import com.softmotions.commons.cont.KVOptions;
 import com.softmotions.commons.io.DirUtils;
-import com.softmotions.ncms.NcmsEnvironment;
 import com.softmotions.ncms.media.MediaReader;
+import com.softmotions.ncms.media.MediaResource;
 import com.softmotions.weboot.mb.MBDAOSupport;
 
 /**
@@ -32,41 +46,121 @@ import com.softmotions.weboot.mb.MBDAOSupport;
 @Path("adm/rs/x/js")
 public class JsRS extends MBDAOSupport {
 
+    private static final Logger log = LoggerFactory.getLogger(JsRS.class);
+
     private static final Pattern HASH_REGEXP = Pattern.compile("^[0-9a-f]{32}(\\.js)?$");
 
-    private final NcmsEnvironment env;
+    private static final Striped<ReadWriteLock> RW_STRIPES = Striped.lazyWeakReadWriteLock(256);
 
     private final File jsCache;
 
+    private final MediaReader mediaReader;
+
+
     @Inject
     public JsRS(SqlSession sess,
-                NcmsEnvironment env,
                 MediaReader mediaReader) throws IOException {
         super(JsRS.class.getName(), sess);
-        this.env = env;
+        this.mediaReader = mediaReader;
         this.jsCache = new File(mediaReader.getBaseDir(), ".jscache");
         DirUtils.ensureDir(jsCache, true);
     }
 
-    @Path("/script/{hash}")
-    public Response script(@PathParam("hash") String hash) {
-        Matcher m = HASH_REGEXP.matcher(hash);
+    /**
+     * Retrieve compiled script by its fingerprint
+     */
+    @Path("/script/{fingerprint}")
+    @Produces("application/javascript;charset=UTF-8")
+    public String script(@PathParam("fingerprint") String fingerprint) throws Exception {
+        Matcher m = HASH_REGEXP.matcher(fingerprint);
         if (!m.matches()) {
             throw new BadRequestException();
         }
         if (m.group(1) == null) {
-            hash += ".js";
+            fingerprint += ".js";
         }
-        File jsFile = new File(jsCache, hash);
-        if (!jsFile.exists()) {
+        File jsFile = new File(jsCache, fingerprint);
+        String data = null;
+        ReadWriteLock rwlock = RW_STRIPES.get(fingerprint);
+        Lock lock = rwlock.readLock();
+        try {
+            if (jsFile.exists()) {
+                data = FileUtils.readFileToString(jsFile, "utf-8");
+            }
+        } finally {
+            lock.unlock();
+        }
+        if (data == null) {
+            lock = rwlock.writeLock();
+            try {
+                if (jsFile.exists()) {
+                    data = FileUtils.readFileToString(jsFile, "utf-8");
+                } else {
+                    data = recompileScript(fingerprint);
+                }
+            } finally {
+                lock.unlock();
+            }
+        }
+        if (data == null) {
             throw new NotFoundException();
         }
-        return Response.ok((StreamingOutput) o -> {
-            try (InputStream is = new FileInputStream(jsFile)) {
-                IOUtils.copyLarge(is, o);
-                o.flush();
+        return data;
+    }
+
+    @Nullable
+    String recompileScript(String fingerprint) throws Exception {
+        String spec = selectOne("selectScriptSpec", fingerprint);
+        return spec != null ? compileScript(fingerprint, new KVOptions(spec)) : null;
+    }
+
+    @Nullable
+    String compileScript(String hash, KVOptions spec) throws Exception {
+        spec = (KVOptions) spec.clone();
+        Long[] ids = Arrays.stream(spec.getOrDefault("scripts", "")
+                                       .split(",")).map(Long::parseLong)
+                           .toArray(Long[]::new);
+
+        spec.remove("scripts");
+
+        List<SourceFile> externs = new ArrayList<>();
+        externs.addAll(CommandLineRunner.getBuiltinExterns(CompilerOptions.Environment.BROWSER));
+        List<SourceFile> inputs = new ArrayList<>(ids.length);
+
+        for (int i = 0; i < ids.length; ++i) {
+            MediaResource meta = mediaReader.findMediaResource(ids[i], null);
+            if (meta == null) {
+                String path = selectOne("selectScriptPathById", ids[i]);
+                if (path == null) {
+                    path = ids[i].toString();
+                }
+                log.error("Missing JS resource: {} hash: {} " +
+                          "Script file will be compiled without this resource",
+                          path, hash);
+            } else {
+                inputs.add(SourceFile.fromCode(meta.getName(), meta.getSource()));
             }
-        }).type("application/javascript;charset=UTF-8").build();
+        }
+
+        CompilerOptions options = new CompilerOptions();
+        Compiler compiler = new Compiler(new ClosureLoggerErrorManager(log));
+
+        options.setOutputCharset(Charset.forName("utf-8"));
+
+        String val = spec.getOrDefault("languageIn", LanguageMode.ECMASCRIPT5.toString());
+        options.setLanguageIn(LanguageMode.valueOf(val));
+
+        val = spec.getOrDefault("languageOut", LanguageMode.ECMASCRIPT5.toString());
+        options.setLanguageOut(LanguageMode.valueOf(val));
+
+        Result result = compiler.compile(externs, inputs, options);
+        if (!result.success) {
+            return null;
+        }
+
+        String data = compiler.toSource();
+        FileUtils.write(new File(jsCache, hash + ".js"), data, "utf-8", false);
+        return data;
     }
 
 
